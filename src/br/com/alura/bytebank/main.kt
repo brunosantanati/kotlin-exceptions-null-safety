051a4b9f2a60ddcb03f2275b4f90e0d0a604c596
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-11 21:16:32 -0300

    Mudar o try catch de lugar e remover ArithmeticException.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,23 +1,20 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.modelo.Endereco
-import java.lang.ArithmeticException
-import java.lang.ClassCastException
 
 fun main() {
     println("início main")
-    try {
-        10 / 0
-    }catch (e: ArithmeticException){
-        println("ArithmeticException foi pegada")
-    }
     funcao1()
     println("fim main")
 }
 
 fun funcao1(){
     println("início funcao1")
-    funcao2()
+    try{
+        funcao2()
+    }catch (e: ClassCastException){
+        println("ClassCastException foi pega")
+    }
     println("fim funcao1")
 }
 
@@ -26,11 +23,7 @@ fun funcao2() {
     for (i in 1..5){
         println(i)
         val endereco = Any()
-        try {
-            endereco as Endereco //casting que gera exception
-        }catch (e: ClassCastException){
-            println("ClassCastException foi pegada")
-        }
+        endereco as Endereco //casting que gera exception
     }
     println("fim funcao2")
 }
