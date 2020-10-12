8680a49a323b1cbd2f094f800b7a618700ca523c
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-11 21:12:25 -0300

    Tratando exceptions com try catch.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,10 +1,16 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.modelo.Endereco
+import java.lang.ArithmeticException
+import java.lang.ClassCastException
 
 fun main() {
     println("início main")
-    10/0
+    try {
+        10 / 0
+    }catch (e: ArithmeticException){
+        println("ArithmeticException foi pegada")
+    }
     funcao1()
     println("fim main")
 }
@@ -20,7 +26,11 @@ fun funcao2() {
     for (i in 1..5){
         println(i)
         val endereco = Any()
-        endereco as Endereco //casting que gera exception
+        try {
+            endereco as Endereco //casting que gera exception
+        }catch (e: ClassCastException){
+            println("ClassCastException foi pegada")
+        }
     }
     println("fim funcao2")
 }
