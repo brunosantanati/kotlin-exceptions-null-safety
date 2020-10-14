7e2eab7a11a099d2e6db9abc52cd258ed8d61577
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-14 13:42:31 -0300

    Comentario e doc sobre Kotlin nao ter checked exceptions.

--- src/br/com/alura/bytebank/exception/FalhaAutenticacaoException.kt
+++ src/br/com/alura/bytebank/exception/FalhaAutenticacaoException.kt
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank.exception
 
+//Kotlin does not have checked exceptions.
+//https://kotlinlang.org/docs/reference/exceptions.html#checked-exceptions
+
 class FalhaAutenticacaoException (
     mensagem: String = "Falha na autenticação"
 ) : Exception(mensagem)
