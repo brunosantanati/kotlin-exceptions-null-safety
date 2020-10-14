0997c91109ba38e997228cbb8429653cd89a1dff
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-14 17:17:39 -0300

    Elvis operator para lancar exception.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,15 +1,17 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.modelo.Endereco
+import java.lang.IllegalStateException
 
 //doc link: https://kotlinlang.org/docs/reference/null-safety.html
 
 fun main() {
-    //var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
-    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa fundos")
+    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    //var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa fundos")
     endereco?.let {
         println(it.logradouro.length)
-        val tamanhoComplemento: Int = it.complemento?.length ?: 0 //Elvis Operator
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
         println(tamanhoComplemento)
     }
 }
