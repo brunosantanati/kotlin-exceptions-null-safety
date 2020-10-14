a0970df604a19dc05a765f4730e39ad5b3dc0644
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-14 16:52:48 -0300

    Outro teste com safe call operator.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -5,23 +5,7 @@ import br.com.alura.bytebank.modelo.Endereco
 //doc link: https://kotlinlang.org/docs/reference/null-safety.html
 
 fun main() {
-    var enderecoNulo: Endereco? = null
-    println(enderecoNulo?.logradouro) //imprime null devido ao uso do safe call operator
-    println(enderecoNulo?.logradouro?.length)
-
-    enderecoNulo?.let {endereco: Endereco ->
-        println("Entrou aqui")
-        println(endereco.logradouro.length)
-    }
-
-    //------------------------------------------------
-
-    var enderecoNaoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
-    println(enderecoNaoNulo?.logradouro)
-    println(enderecoNaoNulo?.logradouro?.length)
-
-    enderecoNaoNulo?.let {endereco: Endereco ->
-        println("Entrou aqui 2")
-        println(endereco.logradouro.length)
-    }
+    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    val logradouro: String? = endereco?.logradouro
+    println(logradouro?.length)
 }
