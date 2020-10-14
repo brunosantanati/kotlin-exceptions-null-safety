e49fcc452410e8e1bf3d3eeac6a440a9466216e9
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-14 17:15:48 -0300

    Usando Elvis Operator.

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -5,7 +5,11 @@ import br.com.alura.bytebank.modelo.Endereco
 //doc link: https://kotlinlang.org/docs/reference/null-safety.html
 
 fun main() {
-    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
-    val logradouro: String? = endereco?.logradouro
-    println(logradouro?.length)
+    //var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa fundos")
+    endereco?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int = it.complemento?.length ?: 0 //Elvis Operator
+        println(tamanhoComplemento)
+    }
 }
