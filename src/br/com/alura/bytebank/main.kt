72778a78192a5db3fc0d0feeb8d5a169028f7ce5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-14 17:23:42 -0300

    Exemplo de safe cast ( as? ).

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -6,12 +6,28 @@ import java.lang.IllegalStateException
 //doc link: https://kotlinlang.org/docs/reference/null-safety.html
 
 fun main() {
-    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
-    //var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa fundos")
+    //var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    var endereco: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa fundos")
     endereco?.let {
         println(it.logradouro.length)
         val tamanhoComplemento: Int =
             it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
         println(tamanhoComplemento)
     }
+
+    fazCastSeguro(1)
+    fazCastSeguro("a") //imprime null
+
+    fazCast(1)
+    fazCast("a") //faz lançar ClassCastException
+}
+
+fun fazCastSeguro(valor: Any){
+    val numero: Int? = valor as? Int
+    println(numero)
+}
+
+fun fazCast(valor: Any){
+    val numero: Int = valor as Int
+    println(numero)
 }
